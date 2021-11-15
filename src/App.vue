42eb7dc6f59890b4118285151969a5f037b796e9
Obez99 <giuliogx@live.it>
2021-11-15 16:21:17 +0100

    Styles single card

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,26 @@
     <header class="px-3 py-2">
       <img src="@/assets/logo.png" alt="logo" />
     </header>
+    <main>
+      <div class="container my-5">
+        <div class="row row-cols-1 row-cols-md-5 g-5">
+          <div class="col">
+            <div class="card p-3">
+              <img class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title text-white">Test</h5>
+                <p class="card-text text-secondary">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
+                  velit nihil voluptas mollitia quas accusamus possimus eius
+                  numquam! Officiis impedit adipisci aspernatur alias! Ipsum,
+                  quaerat nesciunt dolore a similique veritatis?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -15,4 +35,5 @@ export default {
 <style lang="scss">
 @import "@/styles/app";
 @import "@/styles/header.scss";
+@import "@/styles/variables.scss";
 </style>
