9a5ef6102a05d1a269f88b6d0d9c62f691567ba6
Obez99 <giuliogx@live.it>
2021-11-15 21:06:31 +0100

    (Final + bonus) Adds LoadingScreen component

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="loading-screen">
+    <img src="../assets/logo.png" alt="" />
+    <div class="loader">
+      <div class="point point1"></div>
+      <div class="point point2"></div>
+      <div class="point point3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingScreen",
+};
+</script>
+
+<style lang="scss">
+@import "../styles/loading.scss";
+</style>
--- src/components/SongsContainer.vue
+++ src/components/SongsContainer.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="container my-5">
-    <div class="loading-screen" v-if="!isLoaded">
-      <img src="../assets/logo.png" alt="" />
-      <div class="loader">
-        <div class="point point1"></div>
-        <div class="point point2"></div>
-        <div class="point point3"></div>
-      </div>
-    </div>
+    <LoadingScreen v-if="!isLoaded"></LoadingScreen>
     <div class="row row-cols-1 row-cols-md-5 g-5">
       <div class="col" v-for="(song, i) in songsList" :key="i">
         <SongCard
@@ -24,8 +17,12 @@
 <script>
 import axios from "axios";
 import SongCard from "./SongCard.vue";
+import LoadingScreen from "./LoadingScreen.vue";
 export default {
-  components: { SongCard },
+  components: {
+    SongCard,
+    LoadingScreen,
+  },
   name: "SongsContainer",
   data() {
     return {
@@ -47,7 +44,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-@import "@/styles/loading.scss";
-</style>
