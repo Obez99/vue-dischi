4f2af301e324cb7c611a551316a9aed385a9dfec
Obez99 <giuliogx@live.it>
2021-11-15 16:29:43 +0100

    Adds "SongsContainer" component

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,24 @@
       <img src="@/assets/logo.png" alt="logo" />
     </header>
     <main>
-      <div class="container my-5">
-        <div class="row row-cols-1 row-cols-md-5 g-5">
-          <div class="col">
-            <div class="card p-3">
-              <img class="card-img-top" />
-              <div class="card-body">
-                <h5 class="card-title text-white">Test</h5>
-                <p class="card-text text-secondary">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
-                  velit nihil voluptas mollitia quas accusamus possimus eius
-                  numquam! Officiis impedit adipisci aspernatur alias! Ipsum,
-                  quaerat nesciunt dolore a similique veritatis?
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SongsContainer></SongsContainer>
     </main>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import SongsContainer from "./components/SongsContainer.vue";
 export default {
+  components: { SongsContainer },
   name: "App",
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+        console.log(response.data);
+      });
+  },
 };
 </script>
 
---
+++ src/components/SongsContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container my-5">
+    <div class="row row-cols-1 row-cols-md-5 g-5">
+      <div class="col">
+        <div class="card p-3">
+          <img class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title text-white">Test</h5>
+            <p class="card-text text-secondary">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut
+              velit nihil voluptas mollitia quas accusamus possimus eius
+              numquam! Officiis impedit adipisci aspernatur alias! Ipsum,
+              quaerat nesciunt dolore a similique veritatis?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SongsContainer",
+};
+</script>
