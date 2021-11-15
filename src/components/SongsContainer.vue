0a888fe1c930243de522c8ba7ec3b4adc8e54f80
Obez99 <giuliogx@live.it>
2021-11-15 18:56:36 +0100

    Creates loading page

--- src/components/SongsContainer.vue
+++ src/components/SongsContainer.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="container my-5">
+    <div class="loading-screen">
+      <img src="../assets/logo.png" alt="" />
+      <div class="loader">
+        <div class="point point1"></div>
+        <div class="point point2"></div>
+        <div class="point point3"></div>
+      </div>
+    </div>
     <div class="row row-cols-1 row-cols-md-5 g-5">
       <div class="col" v-for="(song, i) in songsList" :key="i">
         <SongCard
@@ -33,3 +41,7 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import "@/styles/loading.scss";
+</style>
