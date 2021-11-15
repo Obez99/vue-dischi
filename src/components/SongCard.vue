2250e9a4c79bea1c01555558dbf0e89fff4e7622
Obez99 <giuliogx@live.it>
2021-11-15 17:51:33 +0100

    Finishes exercise

---
+++ src/components/SongCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card p-3 h-100">
+    <img class="card-img-top" :src="image" />
+    <div class="card-body text-center">
+      <h5 class="card-title text-white text-uppercase">
+        {{ title }}
+      </h5>
+      <p class="card-text text-secondary mb-0">
+        {{ author }}
+      </p>
+      <p class="card-text text-secondary">
+        {{ year }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SongCard",
+  props: {
+    image: String,
+    title: String,
+    author: String,
+    year: String,
+  },
+};
+</script>
--- src/components/SongsContainer.vue
+++ src/components/SongsContainer.vue
@@ -2,20 +2,12 @@
   <div class="container my-5">
     <div class="row row-cols-1 row-cols-md-5 g-5">
       <div class="col" v-for="(song, i) in songsList" :key="i">
-        <div class="card p-3 h-100">
-          <img class="card-img-top" :src="song.poster" />
-          <div class="card-body text-center">
-            <h5 class="card-title text-white text-uppercase">
-              {{ song.title }}
-            </h5>
-            <p class="card-text text-secondary mb-0">
-              {{ song.author }}
-            </p>
-            <p class="card-text text-secondary">
-              {{ song.year }}
-            </p>
-          </div>
-        </div>
+        <SongCard
+          :image="song.poster"
+          :title="song.title"
+          :author="song.author"
+          :year="song.year"
+        ></SongCard>
       </div>
     </div>
   </div>
@@ -23,7 +15,9 @@
 
 <script>
 import axios from "axios";
+import SongCard from "./SongCard.vue";
 export default {
+  components: { SongCard },
   name: "SongsContainer",
   data() {
     return {
